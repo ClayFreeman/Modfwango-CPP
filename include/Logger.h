9f4aa2a7d5f37c535fcf44ffd82a3f6f1e62a746
Clay Freeman <git@clayfreeman.com>
2015-03-04 01:34:00 -0600

    Fix alignment

--- include/Logger.h
+++ include/Logger.h
@@ -12,12 +12,12 @@
 #define _LOGGER_H
 
 // Add these numbers to combine modes
-#define LOG_SILENT  0  // [0x0000] Silent execution
-#define LOG_INFO    1  // [0x0001] Informational output
-#define LOG_STACK   2  // [0x0010] Shows entrance and exit of functions
-#define LOG_DEBUG   4  // [0x0100] Output related to code workflow
-#define LOG_DEVEL   8  // [0x1000] Output for data/resource debugging
-#define LOG_ALL     15 // [0x1111] Show all output
+#define LOG_SILENT 0  // [0x0000] Silent execution
+#define LOG_INFO   1  // [0x0001] Informational output
+#define LOG_STACK  2  // [0x0010] Shows entrance and exit of functions
+#define LOG_DEBUG  4  // [0x0100] Output related to code workflow
+#define LOG_DEVEL  8  // [0x1000] Output for data/resource debugging
+#define LOG_ALL    15 // [0x1111] Show all output
 
 // Used internally for console color codes
 #define COLOR_INFO  "\x1b[32;01m" // Green

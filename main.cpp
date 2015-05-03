52e22c01f4d98aa796e6e99ce8080662a7706571
Clay Freeman <git@clayfreeman.com>
2015-05-03 02:33:27 -0500

    Fork and sleep.

--- main.cpp
+++ main.cpp
@@ -38,7 +38,7 @@ int main(int argc, char* const argv[]) {
   prepare_environment(argc, argv);
 
   // Sleep for 10 seconds for testing
-  sleep(10);
+  fork(); sleep(10);
 
   // // Welcome via console
   // Logger::info("Welcome to Modfwango!");

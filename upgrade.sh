80771c380b8e4f5b6a9c30302aedded69fc2e8a2
Clay Freeman <git@clayfreeman.com>
2015-05-07 20:39:16 -0500

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -3,5 +3,12 @@
 # Determine the path to .modfwango
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-# Change to ${DIR}, fetch the latest commit, and rebuild
-cd ${DIR} && git pull origin master && make clean all
+# Change to ${DIR}
+cd ${DIR}
+
+# Fetch the latest commit and Makefile
+git pull origin master
+git submodule update --init
+
+# Rebuild Modfwango
+make clean all

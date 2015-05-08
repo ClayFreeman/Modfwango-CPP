9735bf4fb78faf9ad152a917407088dfb93fb4eb
Clay Freeman <git@clayfreeman.com>
2015-05-07 20:41:35 -0500

    Use parallel when running make.

--- upgrade.sh
+++ upgrade.sh
@@ -3,6 +3,10 @@
 # Determine the path to .modfwango
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
+# Determine the number of CPU cores
+CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu || \
+  echo 1)
+
 # Change to ${DIR}
 cd ${DIR}
 
@@ -11,4 +15,4 @@ git pull origin master
 git submodule update --init
 
 # Rebuild Modfwango
-make clean all
+make -j${CORES} clean all
